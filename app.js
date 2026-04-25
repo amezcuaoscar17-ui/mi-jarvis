c8ecd6b0d71bf4c9c0d1980e2d1823d4b955a6ca
amezcuaoscar17-ui <amezcuaoscar17@gmail.com>
2026-04-24 23:12:03 -0600

    embed keys

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const API_KEY = CONFIG.GROQ_KEY;
-const GEMINI_KEY = CONFIG.GEMINI_KEY;
-const SERP_KEY = CONFIG.SERP_KEY;
-const SPOTIFY_CLIENT_ID = CONFIG.SPOTIFY_CLIENT_ID;
-const SPOTIFY_REDIRECT = CONFIG.SPOTIFY_REDIRECT;
-const ELEVEN_KEY = CONFIG.ELEVEN_KEY;
+const API_KEY = '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';
+const GEMINI_KEY = 'AIzaSyATGyskwb-pU9msXgXNYeFRzCZja-TsGpI';
+const SERP_KEY = 'b23b2490ed9fab4f3470cb6c52492b1aadf0a741a6302c2d0011caa4e9355338';
+const SPOTIFY_CLIENT_ID = 'c2a50a27968a4e7d812839db2139d543';
+const SPOTIFY_REDIRECT = 'https://amezcuaoscar17-ui.github.io/mi-jarvis/callback';
+const ELEVEN_KEY = 'sk_aeb850900815f0f3d00e458e9d647087c642f30b88bdea8c';
 const ELEVEN_VOICE_ID = 'pNInz6obpgDQGcFmaJgB';
 
 // ── Historial de conversaciones ───────────────────────
