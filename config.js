ad3eba4cdfcf08746f000c615ba1dd18e3b194a3
amezcuaoscar17-ui <amezcuaoscar17@gmail.com>
2026-04-24 22:59:58 -0600

    fix app y config

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  GROQ_KEY: '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',
+  GEMINI_KEY: 'AIzaSyATGyskwb-pU9msXgXNYeFRzCZja-TsGpI',
+  ELEVEN_KEY: 'sk_aeb850900815f0f3d00e458e9d647087c642f30b88bdea8c',
+  SERP_KEY: 'b23b2490ed9fab4f3470cb6c52492b1aadf0a741a6302c2d0011caa4e9355338',
+  SPOTIFY_CLIENT_ID: 'c2a50a27968a4e7d812839db2139d543',
+  SPOTIFY_REDIRECT: 'https://amezcuaoscar17-ui.github.io/mi-jarvis/callback',
+};
